bd0881d1763ec37f9ca54d11be51543607a429b6
juaco1993 <juaco1993@gmail.com>
2020-05-10 18:33:58 -0300

    Commit Inicial

---
+++ Assets/generacion_terreno.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generacion_terreno : MonoBehaviour
+{
+    public GameObject[] tiles;
+    public int columns;
+    public int rows;
+    public int maxBlue = 100;
+    public int maxRed = 100;
+    public int maxYellow = 100;
+    public int maxGreen = 5;
+    public int howManyWhite = 30;
+    private int whiteTileCount;
+    private int greenTileCount;
+    private int redTileCount;
+    private int blueTileCount;
+    private int yellowTileCount;
+    private int tileToGenerate = 0;
+    private int resetHowManyWhite;
+    int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetHowManyWhite = howManyWhite;
+        LoadMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach (Transform child in transform)
+                Destroy(child.gameObject);
+            LoadMap();
+        }
+
+    }
+
+    void LoadMap()
+    {
+        greenTileCount = 0;
+        whiteTileCount = 0;
+        redTileCount = 0;
+        blueTileCount = 0;
+        yellowTileCount = 0;
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < columns; y++)
+            {
+                num = UnityEngine.Random.Range(0, 10);
+                if (x >= 0 && x < 3 && y >= 0 && y < 3)
+                {
+                    tileToGenerate = 1;
+                    CheckTile(tileToGenerate);
+                    GameObject obj;
+                    obj = Instantiate(tiles[tileToGenerate], new Vector3(x, num, y), Quaternion.identity);
+                    obj.transform.parent = transform;
+                }
+                else if (x == rows - 1 && y == columns - 1)
+                {
+                    tileToGenerate = 0;
+                    greenTileCount++;
+                    GameObject obj;
+                    obj = Instantiate(tiles[tileToGenerate], new Vector3(x, num, y), Quaternion.identity);
+                    obj.transform.parent = transform;
+                }
+                else if (x >= rows - 3 && y >= columns - 3)
+                {
+                    tileToGenerate = 1;
+                    CheckTile(tileToGenerate);
+                    GameObject obj;
+                    obj = Instantiate(tiles[tileToGenerate], new Vector3(x, num, y), Quaternion.identity);
+                    obj.transform.parent = transform;
+                }
+                else
+                {
+                    Vector3 pos = new Vector3(x, num, y);
+                    tileToGenerate = Random.Range(0, 5);
+                    CheckTile(tileToGenerate);
+                    GameObject obj;
+                    obj = Instantiate(tiles[tileToGenerate], new Vector3(x, num, y), Quaternion.identity);
+                    obj.transform.parent = transform;
+                }
+
+            }
+        }
+    }
+
+    void CheckTile(int randomTile)
+    {
+        switch (randomTile)
+        {
+            case 0:
+                if (greenTileCount >= maxGreen - 1)
+                {
+                    tileToGenerate = 1;
+                    whiteTileCount++;
+                    howManyWhite--;
+                    break;
+                }
+                else
+                {
+                    if (howManyWhite <= 0)
+                    {
+                        greenTileCount++;
+                        howManyWhite = resetHowManyWhite;
+                        break;
+                    }
+                    else
+                    {
+                        tileToGenerate = 1;
+                        whiteTileCount++;
+                        howManyWhite--;
+                        break;
+                    }
+                }
+            case 2:
+                if (redTileCount >= maxRed)
+                {
+                    tileToGenerate = 1;
+                    whiteTileCount++;
+                    howManyWhite--;
+                    break;
+                }
+                else
+                {
+                    redTileCount++;
+                    break;
+                }
+            case 3:
+                if (blueTileCount >= maxBlue)
+                {
+                    tileToGenerate = 1;
+                    whiteTileCount++;
+                    howManyWhite--;
+                    break;
+                }
+                else
+                {
+                    blueTileCount++;
+                    break;
+                }
+            case 4:
+                if (yellowTileCount >= maxYellow)
+                {
+                    tileToGenerate = 1;
+                    whiteTileCount++;
+                    howManyWhite--;
+                    break;
+                }
+                else
+                {
+                    yellowTileCount++;
+                    break;
+                }
+            default:
+                whiteTileCount++;
+                howManyWhite--;
+                break;
+        }
+    }
+}
---
+++ Assets/movimiento.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+	public float movementSpeed = 2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKey("w")){
+
+    		print("Apretaste W");
+    		transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+
+    	}
+    	if(Input.GetKey("a")){
+
+    		print("Apretaste A");
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed * 2.5f;
+
+        }
+    	if(Input.GetKey("s")){
+
+    		print("Apretaste S");
+    		transform.position += transform.TransformDirection(Vector3.back) * Time.deltaTime * movementSpeed * 2.5f;
+
+    	}
+    	if(Input.GetKey("d")){
+
+    		print("Apretaste D");
+    		transform.position += transform.TransformDirection(Vector3.right) * Time.deltaTime * movementSpeed * 2.5f;
+
+    	}
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Apretaste espacio");
+
+            transform.position += transform.TransformDirection(Vector3.up) * Time.deltaTime * movementSpeed * 2.5f;
+
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            print("Apretaste dasd");
+
+            transform.position += transform.TransformDirection(Vector3.down) * Time.deltaTime * movementSpeed * 2.5f;
+
+        }
+
+    }
+}
